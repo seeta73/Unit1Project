803e8e1bd68640a595b7c769e0167282fe4acc2c
Seeta Genova <99757298+seeta73@users.noreply.github.com>
2026-01-19 19:27:41 +0000

    saving work before pull

--- events/src/components/Calendar.jsx
+++ events/src/components/Calendar.jsx
@@ -19,9 +19,9 @@ const Calendar = () => {
                         <td></td>
                         <td></td>
                         <td></td>
-                        <Event event="1 - Farmers Market" location="Downtown Westlake" color="green" />
+                        <Event event="1 - Farmers Market" location="Downtown Westlake" color="green" link="https://www.instagram.com/whatsupatwestlake/?hl=en" />
                         <td>2</td>
-                        <Event event="3 - Bingo" location="Adventure Park" color="blue" />
+                        <Event event="3 - Bingo" location="Adventure Park" color="blue" link="https://www.instagram.com/whatsupatwestlake/?hl=en" />
                     </tr>
                     <tr>
                         <td>4</td>
@@ -30,34 +30,34 @@ const Calendar = () => {
                         <td>7</td>
                         <td>8</td>
                         <td>9</td>
-                        <Event event="10 - Blood Drive" location="Lodge" color="red" />
+                        <Event event="10 - Blood Drive" location="Lodge" color="red" link="https://www.instagram.com/whatsupatwestlake/?hl=en" />
                     </tr>
                     <tr>
-                        <Event event="11 - Chick-Fil-A" location="Lodge" color="green" />
+                        <Event event="11 - Chick-Fil-A" location="Lodge" color="green" link="https://www.instagram.com/whatsupatwestlake/?hl=en" />
                         <td>12</td>
-                        <Event event="13 - Dance with Me" location="Lodge" color="blue" />
+                        <Event event="13 - Dance with Me" location="Lodge" color="blue" link="https://www.instagram.com/whatsupatwestlake/?hl=en" />
                         <td>14</td>
                         <td>15</td>
-                        <Event event="16 - Karaoke Dinner" location="Downtown Westlake" color="green" />
+                        <Event event="16 - Karaoke Dinner" location="Downtown Westlake" color="green" link="https://www.instagram.com/whatsupatwestlake/?hl=en" />
                         <td>17</td>
                     </tr>
                     <tr>
                         <td>18</td>
                         <td>19</td>
                         <td>20</td>
-                        <Event event="21 - Zumba" location="Lodge" color="blue" />
-                        <Event event="22 - Mobile Clinic" location="Lodge" color="red" />
-                        <Event event="23 - Movie Night" location="Lodge" color="blue" />
+                        <Event event="21 - Zumba" location="Lodge" color="blue" link="https://www.instagram.com/whatsupatwestlake/?hl=en" />
+                        <Event event="22 - Mobile Clinic" location="Lodge" color="red" link="https://www.instagram.com/whatsupatwestlake/?hl=en" />
+                        <Event event="23 - Movie Night" location="Lodge" color="blue" link="https://www.instagram.com/whatsupatwestlake/?hl=en" />
                         <td>24</td>
                     </tr>
                     <tr>
-                        <Event event="25 - Sunday Brunch" location="Town Center" color="green" />
+                        <Event event="25 - Sunday Brunch" location="Town Center" color="green" link="https://www.instagram.com/whatsupatwestlake/?hl=en" />
                         <td>26</td>
                         <td>27</td>
                         <td>28</td>
                         <td>29</td>
                         <td>30</td>
-                        <Event event="31 - Pool Kickoff" location="Adventure Park" color="blue" />
+                        <Event event="31 - Pool Kickoff" location="Adventure Park" color="blue" link="https://www.instagram.com/whatsupatwestlake/?hl=en" />
                     </tr>
 
                 </thead>
--- events/src/components/Event.jsx
+++ events/src/components/Event.jsx
@@ -3,6 +3,7 @@ const Event = (props) => {
         <td className={'Event ' + props.color}>
             <h5>{props.event}</h5>
             <h6>{props.location}</h6>
+            <button className="details" onClick={() => window.open(props.link, '_blank')}>Learn More!</button>
         </td>
     )
 }
